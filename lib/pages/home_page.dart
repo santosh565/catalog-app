7b77957da34a0693724cb34295dbc79355821f96
Santosh Bhattarai <hsotnas76@gmail.com>
2021-03-18 13:34:35 +0545

    load json

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import '../models/catalog.dart';
 import '../widgets/item_widget.dart';
@@ -11,6 +14,20 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    final catalogJson =
+        await rootBundle.loadString("assets/files/catalog.json");
+    final decodedData = jsonDecode(catalogJson);
+    var productsData = decodedData["products"];
+    print(productsData);
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
@@ -19,13 +36,16 @@ class _MyHomePageState extends State<MyHomePage> {
         centerTitle: true,
         title: Text("Catalog App"),
       ),
-      body: ListView.builder(
-        itemCount: CatalogModel.item.length,
-        itemBuilder: (context, index) {
-          return ItemWidget(
-            item: CatalogModel.item[index],
-          );
-        },
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: CatalogModel.item.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              item: CatalogModel.item[index],
+            );
+          },
+        ),
       ),
     );
   }

ae2e0702f04c5aee911670b2eb8e63b31ccb2b7e
Santosh Bhattarai <hsotnas76@gmail.com>
2021-03-18 10:39:02 +0545

    drawer ui update

--- lib/widgets/my_drawer.dart
+++ lib/widgets/my_drawer.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class MyDrawer extends StatelessWidget {
@@ -11,9 +12,54 @@ class MyDrawer extends StatelessWidget {
           padding: EdgeInsets.zero,
           children: [
             UserAccountsDrawerHeader(
+                currentAccountPicture: CircleAvatar(
+                  backgroundImage: NetworkImage(
+                      "https://secure.gravatar.com/avatar/c7e3c58d2aa77a0b686f702ccc2717a0?s=800&d=identicon"),
+                ),
                 decoration: BoxDecoration(color: Colors.deepPurple),
-                accountName: Text('data'),
-                accountEmail: Text("data"))
+                accountName: Text(
+                  'Santosh Bhattarai',
+                ),
+                accountEmail: Text("hsotnas76@gmail.com")),
+            ListTile(
+              leading: Icon(
+                CupertinoIcons.home,
+                color: Colors.white,
+              ),
+              title: Text(
+                "Home",
+                textScaleFactor: 1.2,
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                CupertinoIcons.profile_circled,
+                color: Colors.white,
+              ),
+              title: Text(
+                "Profile",
+                textScaleFactor: 1.2,
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                CupertinoIcons.mail,
+                color: Colors.white,
+              ),
+              title: Text(
+                "Email me",
+                textScaleFactor: 1.2,
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            )
           ],
         ),
       ),
